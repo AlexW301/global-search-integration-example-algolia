e8e018442943faf86fcf6b9f927f963ba669a378
Alex <alexwaller301@gmail.com>
2023-10-17 14:20:09 -0400

    Update algoliaItem.ts

--- src/functions/utils/algoliaItem.ts
+++ src/functions/utils/algoliaItem.ts
@@ -40,7 +40,7 @@ export const convertToAlgoliaItem =
     objectID: createObjectId(item.system.codename, item.system.language),
     slug: Object.values(item.elements).find(el => el.type === ElementType.UrlSlug)?.value ?? "",
     content: createRecordBlock(allItems, [], expectedSlug)(item),
-    blogCategory: item.system.type === "blog" ? item.elements.blogCategory.value[0].name : undefined
+    blogCategory: item.system.type === "blog" ? "is blog" : undefined
   });
 
 const createRecordBlock =
